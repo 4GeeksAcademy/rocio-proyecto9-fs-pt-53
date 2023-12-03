574ce46dda4c0e013b6040504d6d8f41fdfc9a53
Rocio <144937216+Rociosantos18@users.noreply.github.com>
2023-12-03 12:34:36 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our", "some"];
+  let adjs = ["great", "big", "cool"];
+  let nouns = ["jogger", "racoon", "blog"];
+  let tlds = [".com", ".org", ".es"];
+
+  let domainNames = [];
+
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let tld of tlds) {
+          domainNames.push(`${pronoun}${adj}${noun}${tld}`);
+        }
+      }
+    }
+  }
+  document.body.innerHTML = `<ul>${domainNames
+    .map(domainNames => `<li>${domainNames}</li>`)
+    .join(" ")}</ul>`;
 };
